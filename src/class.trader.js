3d290878309ca75bf5033602a59e322306ec44cb
Mike Bamber <mike.bamber@procure-plus.com>
2023-11-07 19:02:40 +0000

    getting tests to pass

--- src/class.trader.js
+++ src/class.trader.js
@@ -1,30 +1,30 @@
-const {ERR_INVALID_ARGS} = require("@screeps/common/lib/constants");
 
 class Trader {
     constructor() {
         this.orders = {}; // Store orders in an object with IDs as keys
+        this.orderDups = {};
     }
     
     createOrder(roomName, resourceType, amount) {
 
-        if(!roomName || roomName.length<4 || roomName.length>6)return ERR_INVALID_ARGS;
+        if( !roomName || roomName.length<4 || roomName.length>6 )return ERR_INVALID_ARGS;
+        if( !['W','E'].includes(roomName.charAt(0)) )return ERR_INVALID_ARGS;
         if(amount<1||amount>250000)return ERR_INVALID_ARGS;
+        if(resourceType!==RESOURCE_ENERGY && !REACTIONS[resourceType])return ERR_INVALID_ARGS;
+        if(this.orderDups[roomName+resourceType])return ERR_NAME_EXISTS;
 
         // Create a unique ID for the order based on roomName, resourceType, and Game.time
         const id = `${roomName}_${resourceType}_${Game.time}`;
-        
-        // Create the order object
-        const order = {
-          id,
-          roomName,
-          resourceType,
-          amount,
-          fulfilledAt: null
-        };
-        
+
         // Add the order to the internal orders object
-        this.orders[id] = order;
-        
+        this.orders[id] = {
+            id,
+            roomName,
+            resourceType,
+            amount,
+            fulfilledAt: null
+        };
+        this.orderDups[roomName+resourceType]=true;
         // Return the newly created order
         return id;
     }
--- tests/class.trader.test.js
+++ tests/class.trader.test.js
@@ -2,27 +2,52 @@ const DOC_ROOT = "../";
 global.Game = {time:1}
 const traderClass = require(DOC_ROOT+'src/class.trader');
 const constants = require('@screeps/common/lib/constants');
-const {RESOURCE_UTRIUM, ERR_INVALID_ARGS, RESOURCE_ZYNTHIUM} = require("@screeps/common/lib/constants");
+const {RESOURCE_UTRIUM, ERR_INVALID_ARGS, RESOURCE_ZYNTHIUM, RESOURCE_ENERGY, ERR_NAME_EXISTS} = require("@screeps/common/lib/constants");
 
 // Append each attribute from the module to the global scope
 Object.assign(global, constants);
 
-let trader = new traderClass();
 
+let trader = new traderClass();
 let dataProvider = [
     [0, "Basic happy path", "W1N1", RESOURCE_UTRIUM, 1, "W1N1_U_1"],
-    [1, "Basic happy path", "W1N1", RESOURCE_ZYNTHIUM, 500, "W1N1_Z_1"],
-    [2, "No room name", "", "Z", 500, ERR_INVALID_ARGS],
-    [3, "invalid room name", "a1b1", "Z", 500, ERR_INVALID_ARGS],
-    [4, "no resource", "W1N1", "", 500, ERR_INVALID_ARGS],
-    [5, "bad resource", "W1N1", "A", 500, ERR_INVALID_ARGS],
-    [6, "positive amount", "W1N1", "Z", -1, ERR_INVALID_ARGS],
-    [7, "positive amount", "W1N1", "Z", 0, ERR_INVALID_ARGS],
-    [8, "amount too big, >250k", "W1N1", "Z", 250000, ERR_INVALID_ARGS],
+    [1, "No room name", "", RESOURCE_ENERGY, 500, ERR_INVALID_ARGS],
+    [2, "invalid room name", "a1b1", RESOURCE_ENERGY, 500, ERR_INVALID_ARGS],
+    [3, "no resource", "W1N1", "", 500, ERR_INVALID_ARGS],
+    [4, "bad resource", "W1N1", "A", 500, ERR_INVALID_ARGS],
+    [5, "positive amount <0", "W1N1", RESOURCE_ENERGY, -1, ERR_INVALID_ARGS],
+    [6, "positive amount =0", "W1N1", RESOURCE_ENERGY, 0, ERR_INVALID_ARGS],
+    [7, "amount too big, >250k", "W1N1", RESOURCE_ENERGY, 250001, ERR_INVALID_ARGS],
+    [8, "amount not too big =250k", "W1N1", RESOURCE_ENERGY, 250000, "W1N1_energy_1"],
+    [9, "Same resource. Different room", "W2N1", RESOURCE_ENERGY, 250000, "W2N1_energy_1"],
+    [10, "Same room. Different resource", "W1N1", RESOURCE_ZYNTHIUM, 250000, "W1N1_Z_1"],
 ];
 
-describe.each(dataProvider)('creatOrder()', (ds,description,roomName, resource_type, amount, expected) => {
+
+describe.each(dataProvider)('creatOrder() - Basics', (ds,description,roomName, resource_type, amount, expected) => {
     it(`DS:${ds} ${description} - Given (${roomName},${resource_type},${amount}) >> ${expected}`, () => {
         expect( trader.createOrder(roomName,resource_type,amount) ).toBe(expected);
+    });
+});
+
+
+describe('createOrder() - Advanced', () => {
+    it('DS:0 Same room & resource', () => {
+        let trader = new traderClass();
+        trader.createOrder("W1N1", RESOURCE_UTRIUM, 1);
+        expect(trader.createOrder("W1N1", RESOURCE_UTRIUM, 1)).toBe(ERR_NAME_EXISTS);
+    });
+    it('DS:1 Same room & resource. Different tick', () => {
+        let trader = new traderClass();
+        trader.createOrder("W1N1", RESOURCE_UTRIUM, 1);
+        Game.time++;
+        expect(trader.createOrder("W1N1", RESOURCE_UTRIUM, 1)).toBe(ERR_NAME_EXISTS);
+    });
+    it('DS:2 Valid orders. Different ticks', () => {
+        let trader = new traderClass();
+        Game.time = 1;
+        expect(trader.createOrder("W1N1", RESOURCE_UTRIUM, 1)).toBe( "W1N1_U_1");
+        Game.time++;
+        expect(trader.createOrder("W2N1", RESOURCE_UTRIUM, 1)).toBe( "W2N1_U_2");
     });
 });
