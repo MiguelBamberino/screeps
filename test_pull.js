93d3081de456095075bdce79dc6bbc3430de9d4e
Mike Bamber <mike.bamber@procure-plus.com>
2024-01-24 15:55:36 +0000

    completed pull for mmo

--- test_pull.js
+++ test_pull.js
@@ -1,4 +1,7 @@
 const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
 https.get('https://screeps.com/api/user/code?_token=c8bb2547-cb51-4f0a-b475-213cd12c3705', (resp) => {
     let rawData = '';
     process.stdout.write("Receiving chunk")
@@ -19,7 +22,16 @@ https.get('https://screeps.com/api/user/code?_token=c8bb2547-cb51-4f0a-b475-213c
 
         console.log("Branch:",response.branch);
         console.log("Files:",Object.keys(response.modules).length);
-                console.log(Object.keys(response));
+        console.log("--------------------------------")
+        console.log("Deleting old files...")
+        deleteExistingFiles(path.join(__dirname, 'src'))
+        console.log("--------------------------------")
+        console.log("Saving new files...")
+        for(let fileName in response.modules){
+            writeNewFile(path.join(__dirname, 'src'),fileName+'.js',response.modules[fileName])
+        }
+
+
 
 
     });
@@ -27,3 +39,32 @@ https.get('https://screeps.com/api/user/code?_token=c8bb2547-cb51-4f0a-b475-213c
 }).on("error", (err) => {
     console.log("Error: " + err.message);
 });
+function writeNewFile(directoryPath,fileName,content){
+    let filePath =  path.join(directoryPath, fileName)
+    try {
+        fs.writeFileSync(filePath, content);
+        console.log(fileName,'... \x1b[32m','Written', '\x1b[0m');
+    } catch (err) {
+        console.error(fileName,'... \x1b[31m','Error:', '\x1b[0m',err);
+    }
+}
+function deleteExistingFiles(directoryPath){
+
+    try {
+        const files = fs.readdirSync(directoryPath);
+        files.forEach(fileName => {
+            let filePath =  path.join(directoryPath, fileName)
+            try {
+                fs.unlinkSync(filePath);
+                console.log(fileName,'... \x1b[32m','deleted', '\x1b[0m');
+            } catch (err) {
+                console.error(fileName,'... \x1b[31m','Error:', '\x1b[0m',err);
+            }
+
+        });
+    } catch (err) {
+        console.error('Error reading directory', err);
+    }
+
+
+}
