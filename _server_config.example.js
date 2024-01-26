3766c02e1a06e992e7fe2dbeaf1e8cdb5d9b2d51
Mike Bamber <mikebamber@live.com>
2024-01-26 07:50:41 +0000

    new server conf

--- _server_config.example.js
+++ _server_config.example.js
@@ -1,29 +1,63 @@
 console.log("LOADING... _server_config")
 
 gui.nodeStats = true;
+gui.remoteStats = true;
+gui.speedRunStats = true;
 gui.tradeStats = true;
+gui.nodeSrcStats = true;
+gui.nodeControllerStats =true;
+gui.showDropStats = true;
+logs.runRCLSpeedStats = true;
 // mb.createMapRoute(['W41N53','W40N53','W40N52','W39N52'])
 
 let loader = {
     alpha_id:'',
     loadNodes(){
-        nodes.a  = new RoomNode('Alpha',{});
+        nodes.a  = new RoomNode('Alpha',{buildFast:true,upgradeRate:RATE_VERY_FAST});
     },
     detectRespawn(){
 
         if(Game.spawns['Alpha']){
-            if(this.alpha_id!==Game.spawns['Alpha'].id){
+            if(this.alpha_id && this.alpha_id!==Game.spawns['Alpha'].id){
                 console.log("RESPAWN Detected...")
                 this.alpha_id = Game.spawns['Alpha'].id;
                 this.resetData();
             }
+            this.alpha_id = Game.spawns['Alpha'].id;
         }
 
     },
     resetData(){
         console.log("RESETTING... _server_config")
+
+
+        this.resetBotData()
         this.loadNodes();
+        util.destroyAllConSites();
+        util.destroyAllStructures([STRUCTURE_ROAD,STRUCTURE_CONTAINER]);
+
+        gui.init();
+    },
+    resetBotData(){
+
+        delete Memory.creeps;
+        delete Memory.spawns;
+        delete Memory.mapBook;
+        delete Memory.logs;
+        delete Memory.reservationBook;
+        delete Memory.objectMeta;
+        delete Memory.roomNodes;
+        delete Memory.remotes
+
+        Memory.creeps = {};
+        logs.initiate();
+        mb.initiate();
+        reservationBook.initiate();
+        objectMeta.empty();
+
     }
 };
 module.exports = loader;
 loader.loadNodes();
+
+
