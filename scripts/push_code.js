9980fc883476981c1f306d89fcab241e2d7c0c1a
Mike Bamber <mike.bamber@procure-plus.com>
2024-01-28 19:16:16 +0000

    moving push code scripts to npm scripts

---
+++ scripts/push_code.js
@@ -0,0 +1,120 @@
+
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+
+function pushCodeToServer(){
+
+    const path = './scripts/servers.json';
+
+    if(!process.argv || process.argv.length<3){
+        console.log("ERROR: arg 1 must be a push location, such as mmo|swc|season...");
+        return;
+    }
+    if (!fs.existsSync(path)) {
+        console.log('ERROR: server config does not exist at /scripts/servers.json');
+        return;
+    }
+    let servers={};
+    try{
+
+        let json = fs.readFileSync(path,'utf8');
+        servers = JSON.parse(json);
+    }catch (e) {
+        console.log("ERROR: parsing config file from json.")
+        return;
+    }
+        let serverRef = process.argv[2];
+        let config = servers[ serverRef ];
+        let branch = process.argv[3]?process.argv[3]:'default';
+        let apiPath = config.path?config.path:'/api/user/code'
+        console.log("-------------------------------------------");
+        console.log("Server:",serverRef);
+        console.log("host:",config.hostname,"port:",config.port);
+        console.log("path:",apiPath);
+        console.log("branch:",branch);
+        if(config.token)
+            console.log("Auth: with token");
+        else
+            console.log("Auth: email + password");
+        console.log("-------------------------------------------");
+        let data = {
+                branch: branch,
+                modules: buildModuleObject()
+            };
+        //console.log(data)
+        console.log("-------------------------------------------");
+        let options = {
+            hostname: config.hostname,
+            port: config.port,
+            path: apiPath,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        }
+        if(config.token){
+            //options.path += config.token;
+            options.headers['X-Token'] = config.token;
+        }else{
+            options.auth= config.email + ':' + config.password;
+        }
+
+        console.log("SENDING....");
+        //return;
+        const proto = config.protocol==='http' ? http : https;
+        let req = https.request(options, function(res) {
+            res.setEncoding('utf8');
+            console.log("Response Code:",res.statusCode);
+
+            if(res.statusCode < 200 || res.statusCode >= 300) {
+                console.error('Screeps server returned error code ' + res.statusCode);
+                return;
+            }
+            res.on('data', (d) => {
+                process.stdout.write(d);
+            });
+
+        });
+        // Handle request error
+        req.on('error', (error) => {
+            console.error(error);
+        });
+        req.write(JSON.stringify(data));
+        req.end();
+
+        console.log("-------------------------------------------");
+
+
+
+
+
+}
+
+
+pushCodeToServer();
+function buildModuleObject(){
+    let directoryPath = './src'
+    let modules = {};
+    try {
+        const files = fs.readdirSync(directoryPath);
+        console.log("Reading : ",directoryPath,"...")
+        files.forEach(fileName => {
+            let filePath =  path.join(directoryPath, fileName)
+            try {
+                let modeName = fileName.replace('.js','')
+                modules[modeName] = fs.readFileSync(filePath,'utf8');
+                console.log(fileName,'... \x1b[32m','Read', '\x1b[0m');
+            } catch (err) {
+                console.error(fileName,'... \x1b[31m','Error:', '\x1b[0m',err);
+            }
+
+        });
+    } catch (err) {
+        console.error('ERROR: reading directory:',directoryPath, err);
+        return false;
+    }
+    return modules;
+}
---
+++ scripts/push_code.php
@@ -0,0 +1,56 @@
+<?php
+
+use GuzzleHttp\Utils;
+
+include __DIR__."/vendor/autoload.php";
+
+$serverName = 'https://screeps.com/';
+$client = new \GuzzleHttp\Client(['base_uri' => $serverName/*, 'http_errors' => false*/]);
+$options = [
+    'headers' => ['Accept' => 'application/json','Content-Type'=>'application/json'],
+    'query' => ['_token'=>'c8bb2547-cb51-4f0a-b475-213cd12c3705'],
+];
+
+
+
+
+$files = scandir("src");
+$data = ["branch"=>"V19.1","modules"=>[]];
+$fileData=[];
+if(is_array($files)) {
+    echo "======================\n";
+    echo "Building Post Data... \n";
+    echo "======================\n";
+    echo "reading \e[36m" . count($files) . "\e[0m files ...";
+    foreach ($files as $filename) {
+        $diskName = 'src/'.$filename;
+        $keyName = str_replace('.js','',$filename);
+        if ($filename == '.' || $filename == '..') continue;
+        echo "reading $diskName >> $keyName ...";
+        $data['modules'][ $keyName ] = file_get_contents($diskName);
+        if ($data['modules'][ $keyName ]) {
+            echo "\e[32msuccess\e[0m \n";
+        } else {
+            echo "\e[31mfailed\e[0m \n";
+        }
+    }
+    echo "======================\n";
+    echo "Branch name: [ " . $data['branch'] . " ] \n";
+    echo "Server: ".$serverName."\n";
+    echo "======================\n";
+    echo "sending...\n";
+    //return;
+    //$options['body']='{"branch":"V18.5","modules":{"main":"require(\"hello\");","hello":"console.log(\"Hello World!\");"}}';
+    $options['body'] = Utils::jsonEncode($data);
+   // $options['json'] = $data;
+   // $request = new \GuzzleHttp\Psr7\Request('POST', 'https://screeps.com/api/user/code');
+  // var_dump($data);
+  // return;
+    $response = $client->post('/api/user/code',$options);
+    echo "Response code: ".$response->getStatusCode()."\n";
+    $body = Utils::jsonDecode($response->getBody(), true);
+    echo "Body: ".print_r($body,true)."\n";
+    echo "======================\n";
+}else{
+    echo "<< No code in src to push >>\n";
+}
